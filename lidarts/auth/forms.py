5d5f75fc94baca2f94bec67e1e7f5d7af32cbea3
Michael Engel <michael.engel@ymail.com>
2018-05-16 10:52:36 +0200

    Added simple Login form

---
+++ lidarts/auth/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
--- lidarts/auth/routes.py
+++ lidarts/auth/routes.py
@@ -1,10 +1,15 @@
-from flask import render_template
+from flask import render_template, flash, url_for, redirect
 from lidarts.auth import bp
+from lidarts.auth.forms import LoginForm
 
 
-@bp.route('/login')
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}.'.format(form.username.data))
+        return redirect(url_for('generic.index'))
+    return render_template('auth/login.html', form=form)
 
 
 @bp.route('/register')
