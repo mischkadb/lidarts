30afde13102ff340055a2d48248da306f2c21c90
Michael Engel <michael.engel@ymail.com>
2018-05-23 00:30:52 +0200

    Updated setup.py for deployment

--- setup.py
+++ setup.py
@@ -2,11 +2,26 @@ from setuptools import find_packages, setup
 
 setup(
     name='lidarts',
-    version='0.0.1-alpha',
+    version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
+        'alembic',
+        'coverage',
+        'eventlet',
+        'Flask-Babel',
+        'Flask-Login',
+        'Flask-Mail',
+        'Flask-Migrate',
+        'Flask-SocketIO',
+        'Flask-SQLAlchemy',
+        'Flask-WTF',
+        'gunicorn',
+        'hashids',
+        'psycopg2-binary',
+        'pytest',
+        'python-dotenv',
     ],
 )
