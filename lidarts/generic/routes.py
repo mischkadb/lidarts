230ed888b9cb93e18ed872d6778d2589af0185d5
Michael Engel <michael.engel@ymail.com>
2018-05-28 23:15:36 +0200

    Fixed bug in player overview, status 'challenged' has no player2 name

--- lidarts/generic/routes.py
+++ lidarts/generic/routes.py
@@ -34,10 +34,10 @@ def lobby():
     games_in_progress = Game.query.filter(((Game.player1 == current_user.id) | (Game.player2 == current_user.id)) & \
                                           (Game.status == 'started')).order_by(desc(Game.id)).all()
     for game in games_in_progress:
-        if game.player1 not in player_names:
+        if game.player1 and game.player1 not in player_names:
             player_names[game.player1] = User.query.with_entities(User.username)\
                 .filter_by(id=game.player1).first_or_404()[0]
-        if game.player2 not in player_names:
+        if game.player2 and game.player2 not in player_names:
             player_names[game.player2] = User.query.with_entities(User.username) \
                 .filter_by(id=game.player2).first_or_404()[0]
 
