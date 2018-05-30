f8b0e02cd6dea8a5f79f0198e71f632b9fd44234
Michael Engel <michael.engel@ymail.com>
2018-05-30 18:25:28 +0200

    Fixed bug in session counting

--- lidarts/socket/base_handler.py
+++ lidarts/socket/base_handler.py
@@ -7,13 +7,16 @@ from lidarts.socket.chat_handler import broadcast_online_players
 @socketio.on('connect', namespace='/base')
 def connect():
     print('Client connected', request.sid)
-    current_user.active_sessions += 1
-    db.session.commit()
-    broadcast_online_players()
+    if current_user.is_authenticated:
+        current_user.active_sessions += 1
+        db.session.commit()
+        broadcast_online_players()
 
 
 @socketio.on('disconnect', namespace='/base')
 def disconnect():
     print('Client disconnected', request.sid)
-    current_user.active_sessions -= 1
-    db.session.commit()
+    if current_user.is_authenticated:
+        current_user.active_sessions -= 1
+        db.session.commit()
+        broadcast_online_players()
