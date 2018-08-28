f625d840772918f3e070a0ebebd33c6705015d8d
Michael Engel <michael.engel@ymail.com>
2018-08-28 10:32:05 +0200

    Fixed link to profile in private messages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='lidarts',
-    version='0.4.0-16',
+    version='0.4.0-18',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
