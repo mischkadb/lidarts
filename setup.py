b44882b57bc70123246e368f8c8da3e4163aeec0
Michael Engel <michael.engel@ymail.com>
2020-01-17 20:36:52 +0100

    Push latest version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='lidarts',
-    version='0.6.0-4',
+    version='0.6.0-5',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
