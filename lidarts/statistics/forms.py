ab271bfe9936bf6744e8196864b963c3427511f3
thomasmiko <55090340+thomasmiko@users.noreply.github.com>
2020-01-18 18:42:17 +0100

    Filtering of statistics by match type and opponent implemented

--- lidarts/statistics/forms.py
+++ lidarts/statistics/forms.py
@@ -4,12 +4,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, IntegerField, DateField, SubmitField, HiddenField
 from wtforms.widgets import html5
 from wtforms.validators import DataRequired
+from lidarts.game.forms import game_types, in_choice, out_choice
 
 opponents = [('all', lazy_gettext('All')), ('local', lazy_gettext('Local')),
              ('online', 'Online'), ('computer', 'Computer')]
 
 computer_level = [('all', lazy_gettext('All')), ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'),
-                  ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')]
+                  ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9')]
 
 class StatisticsForm(FlaskForm):
     select_game_range_filter = SelectField('', choices=[('lastgames', lazy_gettext('Last ... games')),
@@ -22,8 +23,11 @@ class StatisticsForm(FlaskForm):
     submit = SubmitField(lazy_gettext('Filter'))
     apply_filter = SubmitField(lazy_gettext('Apply'))
     hiddenfield_selected_tab = HiddenField(id='hiddenFieldSelectedTab')
+    game_types = SelectField(lazy_gettext('Game type'), choices=game_types, validators=[DataRequired()], default="501")
     opponents = SelectField(lazy_gettext('Opponent'), id='selectOpponent', default='all', choices=opponents,
                             validators=[DataRequired()])
     computer_level = SelectField(lazy_gettext('Level'), default='all', choices=computer_level,
                                  validators=[DataRequired()])
     opponent_name = StringField(lazy_gettext('Opponent name'))
+    in_mode = SelectField(lazy_gettext('Mode In'), choices=in_choice, validators=[DataRequired()], default="si")
+    out_mode = SelectField(lazy_gettext('Mode Out'), choices=out_choice, validators=[DataRequired()], default="do")
--- lidarts/statistics/routes.py
+++ lidarts/statistics/routes.py
@@ -33,6 +33,16 @@ def x01():
     custom_filter_date_to = form.date_to.default
 
     # if no value is in the form data, we can insert the default values
+    if form.game_types.data is None:
+        form.game_types.data = form.game_types.default
+    if form.in_mode.data is None:
+        form.in_mode.data = form.in_mode.default
+    if form.out_mode.data is None:
+        form.out_mode.data = form.out_mode.default
+    if form.opponents.data is None:
+        form.opponents.data = form.opponents.default
+    if form.computer_level.data is None:
+        form.computer_level.data = form.computer_level.default
     if form.number_of_games.data is None:
         form.number_of_games.data = custom_filter_number_of_games
     if form.date_from.data is None:
@@ -54,9 +64,39 @@ def x01():
         use_custom_filter_last_games = True
 
     # select the the games (descending order - to be able to filter for last ... games)
-    games = Game.query.filter(((Game.player1 == user.id) | (Game.player2 == user.id))
-                              & (Game.status == 'completed')) \
-        .order_by(desc(Game.begin)).all()
+    query = Game.query.filter(((Game.player1 == user.id) | (Game.player2 == user.id))
+                              & (Game.status == 'completed')
+                              & (Game.type == form.game_types.data)
+                              & (Game.in_mode == form.in_mode.data)
+                              & (Game.out_mode == form.out_mode.data))
+
+    # opponent filter
+    if form.opponents.data == 'local':
+        query = query.filter(Game.opponent_type == 'local')
+    elif form.opponents.data == 'computer':
+        if form.computer_level.data == 'all':
+            # filter for all computer opponents
+            query = query.filter(Game.opponent_type.startswith('computer'))
+        else:
+            # filter for specific computer level
+            query = query.filter(Game.opponent_type == ('computer' + str(form.computer_level.data)))
+    elif form.opponents.data == 'online':
+        query = query.filter(Game.opponent_type == 'online')
+
+        # check if we must filter to games for one specific user
+        if not form.opponent_name.data is None:
+            opponent_user_name = str(form.opponent_name.data)
+            if opponent_user_name:
+                opponent_id = -1
+                # check if username is valid and if the user id is not our own user id
+                opponent_user = User.query.filter(User.username == opponent_user_name).first()
+                if (opponent_user is not None and opponent_user.id is not current_user.id):
+                    opponent_id = opponent_user.id
+
+                query = query.filter((Game.player1 == opponent_id) | (Game.player2 == opponent_id))
+
+    # execute the query
+    games = query.order_by(desc(Game.begin)).all()
 
     # get the dates for date related statistics
     today_date = datetime.today().date()
@@ -172,10 +212,16 @@ def x01():
     # invert the list for averagepergame (we need the date from the oldest game to newest game)
     stats['averagepergame'].reverse()
 
-    active_nav_page = ''
+    computer_level_displaystyle = 'none'
+    opponent_name_displaystyle = 'none'
+
     # if we are in post (from filter), we must append the prefix for the internal link
     if form.is_submitted():
-        active_nav_page = 'custom'
+        if form.opponents.data == 'computer':
+            computer_level_displaystyle = 'block'
+        elif form.opponents.data == 'online':
+            opponent_name_displaystyle = 'block'
 
     return render_template('statistics/x01.html', stats=stats, form=form,
-                           activeNavPage=active_nav_page)
+                           computer_level_displaystyle=computer_level_displaystyle,
+                           opponent_name_displaystyle=opponent_name_displaystyle)
