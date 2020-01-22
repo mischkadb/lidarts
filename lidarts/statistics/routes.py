5d5368b7463a1309a5945f2c0ba6b71247018e25
thomasmiko <55090340+thomasmiko@users.noreply.github.com>
2020-01-22 13:29:27 +0100

    Code refactoring and new stat for shortest leg

--- lidarts/statistics/routes.py
+++ lidarts/statistics/routes.py
@@ -2,216 +2,36 @@
 routing functions for statistics
 """
 
-from datetime import timedelta, datetime
-
-import json
-
 from flask_login import current_user, login_required
 from flask import render_template
-from sqlalchemy import desc
-from lidarts.models import User, Game
+from lidarts.models import User
 from lidarts.statistics import bp
-from lidarts.statistics.utils import calculate_overall_stats_from_leg, sum_up_stats
-from lidarts.statistics.utils import calculate_overall_stats_from_game, create_stats_object
+from lidarts.statistics.utils import create_statistics
 from lidarts.statistics.forms import StatisticsForm
 
 
 @bp.route('/x01', methods=['GET', 'POST'])
 @login_required
 def x01():
+    """ this function get's all required data for the statistics """
     form = StatisticsForm()
 
-    stats = {'today': create_stats_object(), 'currentweek': create_stats_object(),
-             'currentmonth': create_stats_object(), 'currentyear': create_stats_object(),
-             'overall': create_stats_object(), 'custom': create_stats_object(), 'averagepergame': []}
-
     user = User.query.filter(User.id == current_user.id).first_or_404()
-
-    # if (form.is_submitted):
-    custom_filter_number_of_games = form.number_of_games.default
-    custom_filter_date_from = form.date_from.default
-    custom_filter_date_to = form.date_to.default
-
-    # if no value is in the form data, we can insert the default values
-    if form.game_types.data is None:
-        form.game_types.data = form.game_types.default
-    if form.in_mode.data is None:
-        form.in_mode.data = form.in_mode.default
-    if form.out_mode.data is None:
-        form.out_mode.data = form.out_mode.default
-    if form.opponents.data is None:
-        form.opponents.data = form.opponents.default
-    if form.computer_level.data is None:
-        form.computer_level.data = form.computer_level.default
-    if form.number_of_games.data is None:
-        form.number_of_games.data = custom_filter_number_of_games
-    if form.date_from.data is None:
-        form.date_from.data = custom_filter_date_from
-    if form.date_to.data is None:
-        form.date_to.data = custom_filter_date_to
 
     # check which custom filter is active
     use_custom_filter_last_games = False
     use_custom_filter_date_range = False
     if form.select_game_range_filter.data == 'lastgames':
         use_custom_filter_last_games = True
-        custom_filter_number_of_games = form.number_of_games.data
     elif form.select_game_range_filter.data == 'daterange':
         use_custom_filter_date_range = True
-        custom_filter_date_from = form.date_from.data
-        custom_filter_date_to = form.date_to.data
     else:
         use_custom_filter_last_games = True
 
-    # select the the games (descending order - to be able to filter for last ... games)
-    query = Game.query.filter(((Game.player1 == user.id) | (Game.player2 == user.id))
-                              & (Game.status == 'completed')
-                              & (Game.type == form.game_types.data)
-                              & (Game.in_mode == form.in_mode.data)
-                              & (Game.out_mode == form.out_mode.data))
-
-    # opponent filter
-    if form.opponents.data == 'local':
-        query = query.filter(Game.opponent_type == 'local')
-    elif form.opponents.data == 'computer':
-        if form.computer_level.data == 'all':
-            # filter for all computer opponents
-            query = query.filter(Game.opponent_type.startswith('computer'))
-        else:
-            # filter for specific computer level
-            query = query.filter(Game.opponent_type == ('computer' + str(form.computer_level.data)))
-    elif form.opponents.data == 'online':
-        query = query.filter(Game.opponent_type == 'online')
-
-        # check if we must filter to games for one specific user
-        if not form.opponent_name.data is None:
-            opponent_user_name = str(form.opponent_name.data)
-            if opponent_user_name:
-                opponent_id = -1
-                # check if username is valid and if the user id is not our own user id
-                opponent_user = User.query.filter(User.username == opponent_user_name).first()
-                if (opponent_user is not None and opponent_user.id is not current_user.id):
-                    opponent_id = opponent_user.id
-
-                query = query.filter((Game.player1 == opponent_id) | (Game.player2 == opponent_id))
-
-    # execute the query
-    games = query.order_by(desc(Game.begin)).all()
-
     # get the dates for date related statistics
-    today_date = datetime.today().date()
-    start_current_week = today_date - timedelta(days=today_date.weekday())
-    start_current_month = today_date.replace(day=1)
+    stats = create_statistics(user, form, use_custom_filter_last_games, use_custom_filter_date_range)
 
-    number_of_games = 0
-    # iterate through each game
-    for game in games:
-        # hack for legacy games on lidarts.org which do not have double-missed and finish darts
-        if game.id < 18:
-            continue
-
-        game_begin_date = game.begin.date()
-
-        player = '1'
-        if not user.id == game.player1:
-            player = '2'
-
-        number_of_games += 1
-
-        # check if game is valid for the current custom filter settings
-        valid_game_for_custom_filter = False
-        if use_custom_filter_last_games and number_of_games <= custom_filter_number_of_games:
-            valid_game_for_custom_filter = True
-        elif use_custom_filter_date_range and custom_filter_date_from <= game_begin_date <= custom_filter_date_to:
-            valid_game_for_custom_filter = True
-
-        # custom filter
-        if valid_game_for_custom_filter:
-            calculate_overall_stats_from_game(stats['custom'], game, player)
-
-        # use for today statistics?
-        if game_begin_date == today_date:
-            calculate_overall_stats_from_game(stats['today'], game, player)
-
-        # use current week statistics?
-        if game_begin_date >= start_current_week:
-            calculate_overall_stats_from_game(stats['currentweek'], game, player)
-
-        # use current month statistics?
-        if game_begin_date >= start_current_month:
-            calculate_overall_stats_from_game(stats['currentmonth'], game, player)
-
-        # use current year statistics?
-        if game_begin_date.year >= today_date.year:
-            calculate_overall_stats_from_game(stats['currentyear'], game, player)
-
-        # overall stats
-        calculate_overall_stats_from_game(stats['overall'], game, player)
-
-        match_json = json.loads(game.match_json)
-
-        # calculate current game stats
-        current_game_stats = {'darts_thrown': 0, 'total_score': 0}
-
-        # iterate through each set/leg
-        for current_set in match_json:
-            for current_leg in match_json[current_set]:
-                current_player_leg_stats = match_json[current_set][current_leg][player]
-
-                # add stats for current game
-                current_game_stats['darts_thrown'] += len(
-                    current_player_leg_stats['scores']) * 3
-                current_game_stats['total_score'] += sum(
-                    current_player_leg_stats['scores'])
-                if 'to_finish' in current_player_leg_stats:
-                    current_game_stats['darts_thrown'] -= (
-                        3 - current_player_leg_stats['to_finish'])
-
-                # custom filter
-                if valid_game_for_custom_filter:
-                    calculate_overall_stats_from_leg(
-                        stats['custom'], current_player_leg_stats)
-
-                # use for today statistics?
-                if game_begin_date == today_date:
-                    calculate_overall_stats_from_leg(
-                        stats['today'], current_player_leg_stats)
-
-                # use current week statistics?
-                if game_begin_date >= start_current_week:
-                    calculate_overall_stats_from_leg(
-                        stats['currentweek'], current_player_leg_stats)
-
-                # use current month statistics?
-                if game_begin_date >= start_current_month:
-                    calculate_overall_stats_from_leg(
-                        stats['currentmonth'], current_player_leg_stats)
-
-                # use current year statistics?
-                if game_begin_date.year >= today_date.year:
-                    calculate_overall_stats_from_leg(
-                        stats['currentyear'], current_player_leg_stats)
-
-                # overall stats
-                calculate_overall_stats_from_leg(
-                    stats['overall'], current_player_leg_stats)
-
-        # sum up average per game stats
-        game_average = round((current_game_stats['total_score'] / (current_game_stats['darts_thrown'])) * 3, 2) \
-            if current_game_stats['darts_thrown'] else 0
-        stats['averagepergame'].append(game_average)
-
-    # sum up all the stats
-    sum_up_stats(stats['custom'])
-    sum_up_stats(stats['today'])
-    sum_up_stats(stats['currentweek'])
-    sum_up_stats(stats['currentmonth'])
-    sum_up_stats(stats['currentyear'])
-    sum_up_stats(stats['overall'])
-
-    # invert the list for averagepergame (we need the date from the oldest game to newest game)
-    stats['averagepergame'].reverse()
-
+    # style for some ui elements
     computer_level_displaystyle = 'none'
     opponent_name_displaystyle = 'none'
 
--- lidarts/statistics/utils.py
+++ lidarts/statistics/utils.py
@@ -2,6 +2,12 @@
 utility functions for statistics
 """
 
+import json
+
+from datetime import timedelta, datetime
+from sqlalchemy import desc
+from flask_login import current_user
+from lidarts.models import User, Game
 
 def sum_up_stats(stats):
     """
@@ -85,7 +91,7 @@ def create_stats_object():
     """
     return {'darts_thrown': 0, 'double_thrown': 0, 'legs_won': 0, 'total_score': 0, 'average': 0,
             'first9_scores': [], 'first9_average': 0, 'doubles': 0, 'number_of_games': 0,
-            'number_of_legs': 0, 'legs_lost': 0, 'first6_scores': [], 'first6_average': 0,
+            'shortest_leg': 0, 'number_of_legs': 0, 'legs_lost': 0, 'first6_scores': [], 'first6_average': 0,
             'first3_scores': [], 'first3_average': 0, 'twenty_and_more': 0, 'forty_and_more': 0,
             'sixty_and_more': 0, 'games_won': 0, 'games_lost': 0, 'games_draw': 0,
             'games_won_percent': 0, 'games_lost_percent': 0, 'games_draw_percent': 0,
@@ -107,6 +113,9 @@ def create_stats_object():
 
 
 def calculate_overall_stats_from_game(current_stats, game, player):
+    """
+    this function calculates the overall stats from for the current game
+    """
     current_stats['number_of_games'] += 1
 
     # draw?
@@ -182,6 +191,10 @@ def calculate_overall_stats_from_leg(current_stats, match_player_legstats_json):
         current_stats['double_thrown'] += 1
         current_stats['legs_won'] += 1
 
+        # check if we can update the shortest leg
+        if (current_stats['shortest_leg'] == 0 or current_stats['shortest_leg'] > darts_thrown_this_leg):
+            current_stats['shortest_leg'] = darts_thrown_this_leg
+
         if score > current_stats['highest_finish']:
             current_stats['highest_finish'] = score
         if 2 <= score <= 40:
@@ -207,3 +220,172 @@ def calculate_overall_stats_from_leg(current_stats, match_player_legstats_json):
     current_stats['darts_thrown'] += darts_thrown_this_leg
 
     return current_stats
+
+def calculate_set_leg_statistics(match_json, player, current_game_stats, valid_game_for_custom_filter, stats,
+                                 game_begin_date, today_date, start_current_week, start_current_month):
+    """
+    this function calculates the set and leg statistics for a match
+    """
+    for current_set in match_json:
+        for current_leg in match_json[current_set]:
+            current_player_leg_stats = match_json[current_set][current_leg][player]
+
+            # add stats for current game
+            current_game_stats['darts_thrown'] += len(
+                current_player_leg_stats['scores']) * 3
+            current_game_stats['total_score'] += sum(current_player_leg_stats['scores'])
+            if 'to_finish' in current_player_leg_stats:
+                current_game_stats['darts_thrown'] -= (3 - current_player_leg_stats['to_finish'])
+
+            # custom filter
+            if valid_game_for_custom_filter:
+                calculate_overall_stats_from_leg(stats['custom'], current_player_leg_stats)
+
+            # use for today statistics?
+            if game_begin_date == today_date:
+                calculate_overall_stats_from_leg(stats['today'], current_player_leg_stats)
+
+            # use current week statistics?
+            if game_begin_date >= start_current_week:
+                calculate_overall_stats_from_leg(stats['currentweek'], current_player_leg_stats)
+
+            # use current month statistics?
+            if game_begin_date >= start_current_month:
+                calculate_overall_stats_from_leg(stats['currentmonth'], current_player_leg_stats)
+
+            # use current year statistics?
+            if game_begin_date.year >= today_date.year:
+                calculate_overall_stats_from_leg(stats['currentyear'], current_player_leg_stats)
+
+            # overall stats
+            calculate_overall_stats_from_leg(stats['overall'], current_player_leg_stats)
+
+def create_statistics_query(user, form):
+    """
+    this function builds the database query
+    """
+    # basic query
+    query = Game.query.filter(((Game.player1 == user.id) | (Game.player2 == user.id))
+                              & (Game.status == 'completed')
+                              & (Game.type == form.game_types.data)
+                              & (Game.in_mode == form.in_mode.data)
+                              & (Game.out_mode == form.out_mode.data))
+
+    # check if we need the opponent filter
+    if form.opponents.data == 'local':
+        query = query.filter(Game.opponent_type == 'local')
+    elif form.opponents.data == 'computer':
+        if form.computer_level.data == 'all':
+            # filter for all computer opponents
+            query = query.filter(Game.opponent_type.startswith('computer'))
+        else:
+            # filter for specific computer level
+            query = query.filter(Game.opponent_type == ('computer' + str(form.computer_level.data)))
+    elif form.opponents.data == 'online':
+        query = query.filter(Game.opponent_type == 'online')
+
+        # check if we must filter to games for one specific user
+        opponent_user_name = str(form.opponent_name.data)
+        if opponent_user_name:
+            opponent_id = 0
+            # try to get user from database
+            opponent_user = User.query.filter(User.username == opponent_user_name).first()
+
+            # check if username is valid (found in database) and if the user id is not our own user id
+            if (opponent_user is not None and opponent_user.id != current_user.id):
+                opponent_id = opponent_user.id
+
+            query = query.filter((Game.player1 == opponent_id) | (Game.player2 == opponent_id))
+
+    # execute the query (descending order - to be able to filter for last ... games)
+    return query.order_by(desc(Game.begin))
+
+def create_statistics(user, form, use_custom_filter_last_games, use_custom_filter_date_range):
+    """
+    this function builds the statistics for the given games and filter criterias
+    """
+
+    # build the query and select the the games
+    games = create_statistics_query(user, form).all()
+
+    # create the stats object
+    stats = {'today': create_stats_object(), 'currentweek': create_stats_object(),
+             'currentmonth': create_stats_object(), 'currentyear': create_stats_object(),
+             'overall': create_stats_object(), 'custom': create_stats_object(), 'averagepergame': []}
+
+    # get the dates for date related statistics
+    today_date = datetime.today().date()
+    start_current_week = today_date - timedelta(days=today_date.weekday())
+    start_current_month = today_date.replace(day=1)
+
+    number_of_games = 0
+    # iterate through each game
+    for game in games:
+        # hack for legacy games on lidarts.org which do not have double-missed and finish darts
+        if game.id < 18:
+            continue
+
+        game_begin_date = game.begin.date()
+
+        player = '1'
+        if not user.id == game.player1:
+            player = '2'
+
+        number_of_games += 1
+
+        # check if game is valid for the current custom filter settings
+        valid_game_for_custom_filter = False
+        if use_custom_filter_last_games and number_of_games <= form.number_of_games.data:
+            valid_game_for_custom_filter = True
+        elif use_custom_filter_date_range and form.date_from.data <= game_begin_date <= form.date_to.data:
+            valid_game_for_custom_filter = True
+
+        # custom filter
+        if valid_game_for_custom_filter:
+            calculate_overall_stats_from_game(stats['custom'], game, player)
+
+        # use for today statistics?
+        if game_begin_date == today_date:
+            calculate_overall_stats_from_game(stats['today'], game, player)
+
+        # use current week statistics?
+        if game_begin_date >= start_current_week:
+            calculate_overall_stats_from_game(stats['currentweek'], game, player)
+
+        # use current month statistics?
+        if game_begin_date >= start_current_month:
+            calculate_overall_stats_from_game(stats['currentmonth'], game, player)
+
+        # use current year statistics?
+        if game_begin_date.year >= today_date.year:
+            calculate_overall_stats_from_game(stats['currentyear'], game, player)
+
+        # overall stats
+        calculate_overall_stats_from_game(stats['overall'], game, player)
+
+        match_json = json.loads(game.match_json)
+
+        # calculate current game stats
+        current_game_stats = {'darts_thrown': 0, 'total_score': 0}
+
+        # iterate through each set/leg and calculate the corresponding statistics
+        calculate_set_leg_statistics(match_json, player, current_game_stats, valid_game_for_custom_filter, stats,
+                                     game_begin_date, today_date, start_current_week, start_current_month)
+
+        # sum up average per game stats
+        game_average = round((current_game_stats['total_score'] / (current_game_stats['darts_thrown'])) * 3, 2) \
+            if current_game_stats['darts_thrown'] else 0
+        stats['averagepergame'].append(game_average)
+
+    # sum up all the stats
+    sum_up_stats(stats['custom'])
+    sum_up_stats(stats['today'])
+    sum_up_stats(stats['currentweek'])
+    sum_up_stats(stats['currentmonth'])
+    sum_up_stats(stats['currentyear'])
+    sum_up_stats(stats['overall'])
+
+    # invert the list for averagepergame (we need the date from the oldest game to newest game)
+    stats['averagepergame'].reverse()
+
+    return stats
