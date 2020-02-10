cfb10639f6449c664424146ffe270fe4d6c55c07
Michael Engel <michael.engel@ymail.com>
2020-02-10 13:33:25 +0100

    Flake8 compliance for socket base handler

--- lidarts/socket/base_handler.py
+++ lidarts/socket/base_handler.py
@@ -1,44 +1,48 @@
-# from flask import request
-from lidarts import socketio, db
+# -*- coding: utf-8 -*-
+
+"""Base socket used on entire site.
+
+Needed for notifications, status updates etc.
+"""
+
+from datetime import datetime
+
 from flask_login import current_user
 from flask_socketio import emit, join_room
-from lidarts.socket.utils import send_notification
+
+from lidarts import db, socketio
 from lidarts.models import Notification
-from datetime import datetime
+from lidarts.socket.utils import send_notification
 
 
 @socketio.on('connect', namespace='/base')
 def connect():
-    # print('Client connected', request.sid)
+    """Socket conenction handler.
+
+    Sends new notifications.
+    """
     if current_user.is_authenticated:
         join_room(current_user.username)
-        current_user.active_sessions += 1
         notifications = Notification.query.filter_by(user=current_user.id).all()
         for notification in notifications:
             send_notification(current_user.username, notification.message, notification.author, notification.type)
         db.session.commit()
-        # broadcast_online_players()
 
 
 @socketio.on('user_heartbeat', namespace='/base')
-def connect():
+def heartbeat():
+    """User heartbeat handler.
+
+    User sends heartbeat every few seconds.
+    Used to track when user was last seen for status indication.
+    """
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
-        # this causes issues in chat
-        # broadcast_online_players()
 
 
 @socketio.on('get_status', namespace='/base')
 def get_status():
+    """User status to frontend request handler."""
     if current_user.is_authenticated:
         emit('status_reply', {'status': current_user.status})
-
-
-@socketio.on('disconnect', namespace='/base')
-def disconnect():
-    # print('Client disconnected', request.sid)
-    if current_user.is_authenticated:
-        current_user.active_sessions -= 1
-        db.session.commit()
-        # broadcast_online_players()
