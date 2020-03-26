4dac400b100fd4e055493021bd99133860b80da7
Michael Engel <michael.engel@ymail.com>
2020-03-26 09:49:21 +0100

    Emit online players to user joining the chat

--- lidarts/socket/chat_handler.py
+++ lidarts/socket/chat_handler.py
@@ -11,8 +11,7 @@ import bleach
 @socketio.on('connect', namespace='/chat')
 def connect():
     # print('Client connected', request.sid)
-    # broadcast_online_players()
-    pass
+    broadcast_online_players(broadcast=False)
 
 
 @socketio.on('broadcast_chat_message', namespace='/chat')
