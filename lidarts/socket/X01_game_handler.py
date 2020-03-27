4a0a48aa02bb24d9a8f4425f742caada473a21f7
Michael Engel <michael.engel@ymail.com>
2020-03-27 13:53:28 +0100

    Fix bug: send_score_response at fresh leg was not broadcast

--- lidarts/socket/X01_game_handler.py
+++ lidarts/socket/X01_game_handler.py
@@ -116,9 +116,6 @@ def send_score_response(game, old_score=0, broadcast=False):
 
     computer_game = game.opponent_type.startswith('computer')
 
-    if not p1_current_leg_scores and not p2_current_leg_scores:
-        broadcast = False
-
     room = game.hashid if broadcast else request.sid
 
     emit(
