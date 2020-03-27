27ca9f67236fc23552a15376b5bc2c4aa562e60b
Michael Engel <michael.engel@ymail.com>
2020-03-27 17:33:19 +0100

    Disable notification sound without setting callback

--- lidarts/static/js/base_socket.js
+++ lidarts/static/js/base_socket.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     //     http[s]://<domain>:<port>[/<namespace>]
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace, {transports: ['websocket']});
 
-    var notification_sound_enabled = true;
+    var notification_sound_enabled = false;
     // Event handler for new connections.
     // The callback function is invoked when a connection with the
     // server is established.
