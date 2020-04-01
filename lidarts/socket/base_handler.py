905c08d7d9b80209d1a169a3e8f03c8f3bef7819
Michael Engel <michael.engel@ymail.com>
2020-04-01 19:43:58 +0200

    Add user authentication check in base connection

--- lidarts/socket/base_handler.py
+++ lidarts/socket/base_handler.py
@@ -9,14 +9,15 @@ from datetime import datetime
 
 @socketio.on('connect', namespace='/base')
 def connect_client():
-    current_user.ping()
+    if current_user.is_authenticated:
+        current_user.ping()
 
-    join_room(current_user.username)
-    notifications = Notification.query.filter_by(user=current_user.id).all()
-    for notification in notifications:
-        send_notification(current_user.username, notification.message, notification.author, notification.type, silent=True)
+        join_room(current_user.username)
+        notifications = Notification.query.filter_by(user=current_user.id).all()
+        for notification in notifications:
+            send_notification(current_user.username, notification.message, notification.author, notification.type, silent=True)
 
-    emit('status_reply', {'status': current_user.status})
+        emit('status_reply', {'status': current_user.status})
 
 
 @socketio.on('init', namespace='/base')
