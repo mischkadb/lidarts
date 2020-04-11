e2a4f2d6346ca489ff3e059d64a67637ff6c10fa
Michael Engel <michael.engel@ymail.com>
2020-04-11 18:01:17 +0200

    Add usernames to game API

--- lidarts/api/routes.py
+++ lidarts/api/routes.py
@@ -1,15 +1,21 @@
 from flask import jsonify
 from lidarts.api import bp
-from lidarts.models import Game
+from lidarts.models import Game, User
 from lidarts.game.utils import collect_statistics
 import json
+from sqlalchemy.orm import aliased
 
 
 @bp.route('/game/<hashid>')
 def start(hashid):
-    game = (
+    player1 = aliased(User)
+    player2 = aliased(User)
+    game, p1_name, p2_name = (
         Game.query
+        .join()
         .filter_by(hashid=hashid)
+        .join(player1, Game.player1 == player1.id).add_columns(player1.username)
+        .join(player2, Game.player2 == player2.id, isouter=True).add_columns(player2.username)
         .first_or_404()
     )
 
@@ -43,5 +49,8 @@ def start(hashid):
 
     for stat_key, stat in statistics.items():
         return_dict[stat_key] = stat
+
+    return_dict['p1_name'] = p1_name
+    return_dict['p2_name'] = p2_name
 
     return jsonify(return_dict)
