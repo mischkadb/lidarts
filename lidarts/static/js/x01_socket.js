2a070bbc7ae90e8eb5637f4b743ae6e6cb936a75
Michael Engel <michael.engel@ymail.com>
2020-04-21 18:08:17 +0200

    Add rematch option

--- lidarts/static/js/x01_socket.js
+++ lidarts/static/js/x01_socket.js
@@ -15,6 +15,7 @@ $(document).ready(function() {
     var currentuser_id = $('#user_id').data()['id'];
 
     var game_completed = false;
+    var game_url = $('#game_url').data()['url'];
 
     var hashid = $('#hash_id').data();
     var player1_id = $('#player1_id').data()['id'];
@@ -944,6 +945,26 @@ $(document).ready(function() {
         $('.undo-button').show();
         $('.undo-button-active').hide();
         undo_active = false;
+    });
+
+    $('.rematch-offer').click(function() {
+        socket.emit('send_rematch_offer', {hashid: hashid['hashid']});
+        $('.rematch').hide();
+        $('.rematch-sent').show();
+    });
+
+    socket.on('rematch_offer', function() {
+        $('.rematch-offer').hide();
+        $('.rematch-accept').show();
+    });
+
+    $('.rematch-accept').click(function() {
+        $('.rematch-accept').hide();
+        socket.emit('accept_rematch_offer', {hashid: hashid['hashid']});
+    });
+
+    socket.on('start_rematch', function(msg) {
+        window.location.replace(game_url + msg['hashid']);
     });
 
     $('#appleActivateSound').click(function() {
